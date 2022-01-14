1a9c2e6ffd115608c344fd471f6111514286591e
Girish Madnani <girishmadnani05@gmail.com>
2022-01-14 20:40:34 +0000

    timer countdown

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var remainingTime = 0;
 var score = 0;
+var countdown;
 
 
 function startquiz(){
 
     remainingTime = 100;
-    document.getElementById("remainingTime").innerHTML = remainingTime
-
+    document.getElementById("remainingTime").innerHTML = remainingTime;
+    countdown = setInterval(function(){
+        remainingTime--;
+        document.getElementById("remainingTime").innerHTML = remainingTime;
+        if (remainingTime == 0){
+            clearInterval(countdown);
+        }
+    }, 1000);
+    starting();
 }
