87c4964fe8e54788bc12948dfacc7ed36a2720a6
Girish Madnani <girishmadnani05@gmail.com>
2022-01-15 00:32:54 +0000

    start button functionality and first question display

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var remainingTime = 0;
 var score = 0;
 var countdown;
+var questionIndex = 0
+
+var questions =[{
+    questionTitle: "Whats the full form of js",
+    choices: ["javascript","javas", "j script", "java snake"],
+    answer: "javascript"
+}]
 
 
 function startquiz(){
@@ -15,4 +22,24 @@ function startquiz(){
         }
     }, 1000);
     starting();
+}
+
+function starting(){
+    document.getElementById("homeBody").style.display="none"
+    document.getElementById("quizBody").style.display="block"
+
+    document.getElementById("question").textContent = questions[questionIndex].questionTitle;
+    document.getElementById("choice1").textContent = questions[questionIndex].choices[0];
+    document.getElementById("choice2").textContent = questions[questionIndex].choices[1];
+    document.getElementById("choice3").textContent = questions[questionIndex].choices[2];
+    document.getElementById("choice4").textContent = questions[questionIndex].choices[3];
+
+    questionIndex++;
+    if (questionIndex > questions.length - 1) {
+        return;
+    } 
+
+    var content = "<h2" + questions[questionIndex].question + "<h2>"; 
+    // for (var i= )
+    document.getElementById("quizBody").innerHTML = quizContent;
 }
