661c86cb59f22750d65e2681c05082c5ee714f15
Girish Madnani <girishmadnani05@gmail.com>
2022-01-15 01:34:03 +0000

    score counter and next question

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ var remainingTime = 0;
 var score = 0;
 var countdown;
 var questionIndex = 0
+var score = 0
 
 var questions =[{
     questionTitle: "Whats the full form of js",
     choices: ["javascript","javas", "j script", "java snake"],
     answer: "javascript"
+}, 
+{
+    questionTitle: "Inside which HTML element do we put the JavaScript?",
+    choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+    answer: "d. <script>"
 }]
 
+var choice1 = document.getElementById("choice1")
+var choice2 = document.getElementById("choice2")
+var choice3 = document.getElementById("choice3")
+var choice4 = document.getElementById("choice4")
 
 function startquiz(){
 
@@ -25,8 +35,9 @@ function startquiz(){
 }
 
 function starting(){
-    document.getElementById("homeBody").style.display="none"
-    document.getElementById("quizBody").style.display="block"
+    document.getElementById("homeBody").style.display="none";
+    document.getElementById("quizBody").style.display="block";
+    document.getElementById("middle").style.display="block";
 
     document.getElementById("question").textContent = questions[questionIndex].questionTitle;
     document.getElementById("choice1").textContent = questions[questionIndex].choices[0];
@@ -34,12 +45,50 @@ function starting(){
     document.getElementById("choice3").textContent = questions[questionIndex].choices[2];
     document.getElementById("choice4").textContent = questions[questionIndex].choices[3];
 
-    questionIndex++;
+    document.getElementById("choice1").addEventListener("click", select1);
+    document.getElementById("choice2").addEventListener("click", select2);
+    document.getElementById("choice3").addEventListener("click", select3);
+    document.getElementById("choice4").addEventListener("click", select4);
+
     if (questionIndex > questions.length - 1) {
         return;
     } 
 
-    var content = "<h2" + questions[questionIndex].question + "<h2>"; 
-    // for (var i= )
-    document.getElementById("quizBody").innerHTML = quizContent;
+    
 }
+
+function select1(){
+    console.log("option 1")
+    solution(0);
+}
+function select2(){
+    solution(1);
+}
+function select3(){
+    solution(2);
+}
+function select4(){
+    solution(3);
+}
+
+function solution(choice){
+
+    console.log(questions[questionIndex].choices[choice]);
+
+    if(questions[questionIndex].choices[choice] == questions[questionIndex].answer){
+        console.log("correct")
+        questionIndex++;
+        score++;
+        document.getElementById("CorrectorIncorrect").textContent = "Correct";
+        document.getElementById("score").textContent = score;
+        starting();
+    }
+    else{
+        console.log("wrong")
+        questionIndex++;
+        remainingTime -= 5;
+        document.getElementById("CorrectorIncorrect").textContent = "Incorrect";
+        starting();
+    }
+}
+
